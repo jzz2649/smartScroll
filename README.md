03e19cf514b525098e5923e432039ed5f91c0115
jzz7280 <1563382649@qq.com>
2017-03-25 03:24:10 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# smartScroll
+web移动端浮层滚动阻止window窗体滚动JS/CSS处理
+>本插件参考了张鑫旭的博客[web移动端浮层滚动阻止window窗体滚动JS/CSS处理](http://www.zhangxinxu.com/wordpress/2016/12/web-mobile-scroll-prevent-window-js-css/)
+
+##使用示意
+
+调用smartScroll方法
+``` js
+smartScroll(container, selectorScrollable)
+```
+参数container为浮层或者监测的范围，参数selectorScrollable为滚动元素，都为dom对象
+
+##说明
+
+有问题，欢迎提出
+
---
+++ smartScroll.js
@@ -0,0 +1,77 @@
+function smartScroll(container, selectorScrollable) {
+  if (!selectorScrollable || container.isScroll) {
+    return;
+  }
+
+  var data = {
+    posY: 0,
+    maxscroll: 0
+  };
+
+  var nScroll = false;
+
+  container.addEventListener('touchstart', function(event) {
+    var events = event.touches[0] || event;
+    var elTarget = event.target;
+
+    if (elTarget === selectorScrollable || isChild(selectorScrollable, elTarget)) {
+      nScroll = false;
+    } else if (elTarget === container || isChild(container, elTarget)) {
+      nScroll = true;
+    }
+
+    if (nScroll) {
+      return;
+    }
+
+    data.posY = events.pageY;
+    data.scrollY = selectorScrollable.scrollTop;
+    data.maxscroll = selectorScrollable.scrollHeight - container.clientHeight;
+  });
+
+  container.addEventListener('touchmove', function() {
+    if (data.maxscroll <= 0 || nScroll) {
+      event.preventDefault();
+    }
+
+    var scrollTop = selectorScrollable.scrollTop;
+    var events = event.touches[0] || event;
+    var distanceY = events.pageY - data.posY;
+
+    if (distanceY > 0 && scrollTop == 0) {
+      event.preventDefault();
+      return;
+    }
+
+    if (distanceY < 0 && (scrollTop + 1 >= data.maxscroll)) {
+      event.preventDefault();
+      return;
+    }
+  });
+
+  container.addEventListener('touchend', function() {
+    data.maxscroll = 0;
+  });
+
+  container.isScroll = true;
+
+  function isChild(self, child){
+    var children = self.children;
+    var length = children.length;
+    var child_len = 0;
+    for (var i = 0; i < length; i++) {
+      if (children[i].firstElementChild) {
+        child_len = children[i].children.length;
+        for (var j = 0; j < child_len; j++) {
+          if (isChild(children[i], child)) {
+            return true;
+          }
+        }
+      }
+      if (children[i] === child) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
